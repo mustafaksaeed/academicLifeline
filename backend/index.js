76698d5358876c64053701659bbe5631d64b4728
Mustafa Saeed <mustafaksaeed@gmail.com>
2025-09-11 02:14:32 -0400

    added user routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import twilio from "twilio";
+import VoiceResponse from "twilio";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 const accountSid = process.env.SID;
-const accountToken = process.env.TOKEN;
+const authToken = process.env.TOKEN;
+const client = twilio(accountSid, authToken);
+const twilioNum = process.env.twilioNum;
+const myNum = process.env.myNum;
 
 app.listen(port, () => {
   console.log("listening on port", port);
@@ -15,3 +19,31 @@ app.listen(port, () => {
 app.get("/health", (req, res) => {
   res.send("ok");
 });
+
+/*
+async function createCall() {
+  const call = await client.calls.create({
+    from: twilioNum,
+    to: myNum,
+    url: "http://demo.twilio.com/docs/voice.xml",
+  });
+
+  console.log(call.sid);
+}
+createCall();
+*/
+/*
+demo code works!
+async function createMessage() {
+  const message = await client.messages.create({
+    body: "hello",
+    from: twilioNum,
+    to: myNum,
+  });
+
+  console.log(message.body);
+}
+
+createMessage();
+
+*/

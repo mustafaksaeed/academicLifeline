dbabca064668c2e875fdb26a4fdc304caa7b947c
Mustafa Saeed <mustafaksaeed@gmail.com>
2025-09-23 00:12:25 -0400

    removed the unnecessary hassle of navbar changes will do that later

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,10 @@ import WelcomeNavbar from "./components/WelcomeNavbar";
 import WelcomePage from "./pages/WelcomePage";
 import AuthContext from "./contexts/AuthContext";
 
-function App() {
-  const { currentUser } = useContext(AuthContext);
-  return <>{currentUser ? <UserNavbar /> : <WelcomeNavbar />}</>;
-}
+import React from "react";
+
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;

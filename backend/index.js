08aea6d99e961bcdbd696f6b50a02e8de11bdcca
Mustafa Saeed <mustafaksaeed@gmail.com>
2025-10-18 20:23:12 -0400

    added session uuid generator for ids and middleware

--- backend/index.js
+++ backend/index.js
@@ -6,19 +6,36 @@ dotenv.config();
 import authRoutes from "./routes/authRoutes.js";
 import courseRoutes from "./routes/courseRoutes.js";
 import authorize from "./middleware/authMiddleware.js";
+import session from "express-session";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+
+const uuid = uuidv4();
+app.use(
+  session({
+    name: "SessionCookie",
+    genid: function (req) {
+      console.log("session id created");
+      return genuuid();
+    },
+    secret: uuid,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, expires: 60000 },
+  })
+);
+
 app.listen(port, () => {
   console.log("listening on port", port);
 });
 
 app.use("/api", authRoutes);
 app.use("/courses", authorize, courseRoutes);
-
 app.get("/health", (req, res) => {
   console.log(req.body);
   res.send("ok");

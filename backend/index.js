3744226b6913c8b31c8f596e76ed08c3987d5d45
Mustafa Saeed <mustafaksaeed@gmail.com>
2025-10-27 23:51:34 -0400

    removed unnecessary endpoints, also uninstalled sessions and uuid since i dont need it along with logic

--- backend/index.js
+++ backend/index.js
@@ -6,32 +6,12 @@ dotenv.config();
 import authRoutes from "./routes/authRoutes.js";
 import courseRoutes from "./routes/courseRoutes.js";
 import authorize from "./middleware/authMiddleware.js";
-import session from "express-session";
-import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-
-const uuid = uuidv4();
-app.use(
-  session({
-    secret: uuid,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 3600000, //1 hour
-      httpOnly: true,
-      secure: true,
-    },
-  })
-);
-// router.use((req, res, next) => {
-//   if (!req.session.userID) return res.status(403).send("not logged in");
-//   next();
-// });
 
 app.listen(port, () => {
   console.log("listening on port", port);
@@ -43,15 +23,3 @@ app.get("/health", (req, res) => {
   console.log(req.body);
   res.send("ok");
 });
-
-// app.get("/", (req, res) => {
-//   if (req.session.views) {
-//     req.session.views++;
-//     res.send(`Number of views: ${req.session.views}`);
-//   } else {
-//     req.session.views = 1;
-//     res.send("Welcome to this page for the first time!");
-//   }
-// });
-
-// run();

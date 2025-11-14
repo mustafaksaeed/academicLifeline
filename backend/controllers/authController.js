6ae4660fdc3676ecd691e45a299ca28b6c5a7b13
Mustafa Saeed <mustafaksaeed@gmail.com>
2025-11-13 19:46:49 -0500

    removed firebase will use for next project

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,39 +1,11 @@
-import { getAuth } from "firebase/auth";
-import {
-  createUser,
-  authenticateToken,
-  userExistsCheck,
-  createSession,
-  verifySession,
-} from "../db/functions.js";
 
-import auth from "../firebase/firebase.config.js";
 
 //check is user exists in mongodb and pass the , idk if the user id is the token idk figire that out
 // save the uid, and email not password since firebase handles that
 export const register = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    await userExistsCheck(email);
-    await createUser(email, password);
-  } catch (error) {
-    console.log("error:", error);
-    res.send(error);
-  }
+  
 };
 
 export const login = async (req, res) => {
-  const { idToken, csrfToken } = req.body;
 
-  if (csrfToken !== req.cookies.csrfToken) {
-    res.status(401).send("UNAUTHORIZED REQUEST!");
-  }
-
-  try {
-    await authenticateToken(auth, idToken);
-    await verifySession(auth, csrfToken);
-    return await createSession(auth, idToken, res);
-  } catch (error) {
-    console.log("error", error);
-  }
 };
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,16 +1,7 @@
 import { verifySession } from "../db/functions.js";
 //sessions here
 const authorize = async (req, res, next) => {
-  const sessionCookie = req.cookies.session || "";
-
-  try {
-    await verifySession(sessionCookie);
-    console.log("successful verifying cookie");
-    res.status(200).send({ message: "successful verifying cookie" });
-  } catch (error) {
-    console.log("error", error);
-    res.status(401).send({ message: "error validating session" });
-  }
+  
 };
 
 export default authorize;
